a70a46d0675b3ad764cf92e511adc77036a39819
Ismail Bay <ismail.bay@openforce.com>
2014-01-27 15:21:14 +0100

    added new solutions to problem 2

--- P02.scala
+++ P02.scala
@@ -16,10 +16,22 @@ object P02 {
       case _ :: xs => f1(xs)
     }
 
+    // init selects all elements except the last one
+    def f2[T](list: List[T]): T = list.init.last
+
+    // with index
+    def f3[T](list: List[T]): T = {
+      if (list.length > 1) list(list.length - 2)
+      else throw new NoSuchElementException
+    }
+
+    // TODO: function composing & lifting
 
     val list = List(1, 2, 3, 4)
     println("should print 3")
     println(f0(list))
     println(f1(list))
+    println(f2(list))
+    println(f3(list))
   }
 }
