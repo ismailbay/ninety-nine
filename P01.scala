5d23f7f6d756c8dc137ac2f9fd4449ac15352b09
Ismail Bay <ismail.bay@openforce.com>
2014-01-27 11:40:25 +0100

    added main method to execute the functions

--- P01.scala
+++ P01.scala
@@ -1,20 +1,30 @@
 // P01: Find the last element of a list 
 
-// built-in function
-def f0[T](list: List[T]): T = list.last
+object P01 {
+  def main(args: Array[String]) {
+    // built-in function
+    def f0[T](list: List[T]): T = list.last
 
-// tail recursion
-def f1[T](list: List[T]): T = list match {
-  case h :: Nil => h
-  case _ :: t => f1(t)
-  case _ => throw new NoSuchElementException("list is empty")
-}
+    // tail recursion
+    def f1[T](list: List[T]): T = list match {
+      case h :: Nil => h
+      case _ :: t => f1(t)
+      case _ => throw new NoSuchElementException("list is empty")
+    }
 
-// built-in functions chained
-def f2[T](list: List[T]): T = list.reverse.head
+    // built-in functions chained
+    def f2[T](list: List[T]): T = list.reverse.head
 
-// folding
-def f3[T](list: List[T]): T = {
-  if (list.isEmpty) throw new NoSuchElementException("list is empty")
-  else list.reduce((a, b) => b)
+    // folding
+    def f3[T](list: List[T]): T = {
+      if (list.isEmpty) throw new NoSuchElementException("list is empty")
+      else list.reduce((a, b) => b)
+    }
+
+    println("should print '3'")
+    println(f0(List(1, 2, 3)))
+    println(f1(List(1, 2, 3)))
+    println(f2(List(1, 2, 3)))
+    println(f3(List(1, 2, 3)))
+  }
 }
